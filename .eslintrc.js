644ccc15cfba84409198b6ac65b054fe207e86c0
lanhuigu <2405872445@qq.com>
2023-11-29 19:46:37 +0800

    feat: 10
    新增

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,18 +10,10 @@ module.exports = {
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
-		requireConfigFile:false
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-	extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-    'plugin:prettier/recommended'
-  ],
+  }
 }
