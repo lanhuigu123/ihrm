298777c52d01cbebb3d62f3f978d0f0062d2acd2
lanhuigu <2405872445@qq.com>
2023-12-01 13:53:05 +0800

    feat: 新增axios配置
    新增axios配置

--- src/utils/default.js
+++ src/utils/default.js
@@ -1,1 +1,9 @@
-/**默认状态码封装*/
+/**
+ * 状态码默认封装
+ */
+
+const DEFAULTSTATUS = {
+  /**错误请求 */
+  ERRORPRO: 400
+}
+export default DEFAULTSTATUS
--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -21,9 +21,8 @@
 
 let loadingInstance
 import axios from 'axios'
-
-import { Loading } from 'element-ui'
-
+import { MessageBox, Loading } from 'element-ui'
+import DEFAULTSTATUS from './default'
 const instance = axios.create({
   // 根路径 基准地址
   baseURL: '',
@@ -33,7 +32,7 @@ const instance = axios.create({
   Headers: {
     'Content-type': 'application/json; charset=utf-8'
   },
-  // 跨域请求配置凭证
+  // 表示跨域请求时是否需要使用凭证
   withCredentials: false
 })
 
@@ -51,17 +50,80 @@ instance.interceptors.request.use(
     return Promise.reject(err)
   }
 )
-
 instance.interceptors.response.use(
-  (res) => {
-    // Loading.service({ fullscreen: true, text: '加载中...' }).close()
+  (response) => {
     loadingInstance.close()
-
-    return res.data
+    if (response.data && response.data.status === 2) {
+      // 401, token失效
+      /**
+       * TODO: 401用户登录
+       */
+      //  // resetLoginInfo()
+      //   router.push({
+      //     name: "login"
+      //   })
+    }
+    return response
   },
-  (err) => {
-    // Loading.service({ fullscreen: true, text: '加载中...' }).close()
-    return Promise.reject(err)
+  (error) => {
+    let title = ''
+    let message = ''
+    loadingInstance.close()
+    if (error && error.response) {
+      /**后端返回的报错的信息 */
+      message = error.response.data.message
+      // 401, token失效
+      switch (
+        error.response.status // 跨域存在获取到的状态码的情况, status(随后端定义变化而变化,code)
+      ) {
+        case DEFAULTSTATUS.ERRORPRO:
+          title = '错误请求'
+          break // 停止循环
+        case 401:
+          title = '资源未授权'
+          break
+        case 403:
+          title = '禁止访问'
+          break
+        case 404:
+          title = '未找到所请求的资源'
+          break
+        case 405:
+          title = '不允许使用该方法'
+          break
+        case 408:
+          title = '请求超时'
+          break
+        case 500:
+          title = '内部服务器错误'
+          break
+        case 501:
+          title = '未实现'
+          break
+        case 502:
+          title = '网关错误'
+          break
+        case 503:
+          title = '服务不可用'
+          break
+        case 504:
+          title = '网关超时'
+          break
+        case 505:
+          title = 'HTTP版本不受支持'
+          break
+        default:
+          title = error.response.status
+      }
+      return MessageBox.alert(message, title, {
+        type: 'warning'
+      })
+    } else {
+      // 跨域获取不到状态码或者其他状态进行的处理
+      return MessageBox.alert('请联系系统管理员, 或稍后再试!', '未知错误', {
+        type: 'error'
+      })
+    }
   }
 )
 
@@ -69,3 +131,15 @@ instance.interceptors.response.use(
  * 封装动态代理标识
  * process.env.VUE_APP_IDENT
  */
+
+instance.addURL = function (url) {
+  console.log(url)
+  console.log(process.env.VUE_APP_IDENT + url)
+  return process.env.VUE_APP_IDENT + url
+}
+
+/***
+ * TODO: 封装get和post请求
+ */
+
+export default instance
